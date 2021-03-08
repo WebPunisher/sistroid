7b6e8afbfa04134d6bb1730ecf12fa06ece4cd6f
Ricider <sarp1999@gmail.com>
2021-03-08 23:24:04 +0300

    added few database tables

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask,request
 from flask_mysqldb import MySQL
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
 
@@ -12,23 +13,55 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
-@app.route('/')
-def index():
+@app.route("/create_tables") #uwu
+def create_tables():
     cur = mysql.connection.cursor()
+    
     cur.execute('''CREATE TABLE IF NOT EXISTS people2 (
     person_id INT AUTO_INCREMENT PRIMARY KEY,
     pname VARCHAR(255) NOT NULL,
     psurname VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );''')
+    );''')    
+    
+    cur.execute('''CREATE TABLE IF NOT EXISTS topics (
+    class_id INT AUTO_INCREMENT PRIMARY KEY,
+    class_name VARCHAR(255) NOT NULL,
+    class_desc VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );''')    
+    
+    cur.execute('''CREATE TABLE IF NOT EXISTS classes (
+    crn INT AUTO_INCREMENT PRIMARY KEY,
+    c_teacher_id INT NOT NULL,
+    c_class_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(c_teacher_id)
+    REFERENCES people2 (person_id),
+    FOREIGN KEY(c_class_id)
+    REFERENCES topics (class_id)
+    );''')   
+    
+    mysql.connection.commit()  
+    return("success")
 
+@app.route('/add_user',methods = ["POST"])
+def add_user():
+    if request.method == "POST":
+        data = request.json
+        
+        cur = mysql.connection.cursor()
+        cur.execute('''
+            INSERT INTO people2 (pname,psurname)
+            VALUES ('{}','{}'); 
+        '''.format(data["pname"],data["psurname"]))
+        mysql.connection.commit()        
+        
+        return "added user"
 
-    # cur.execute('''
-    #     INSERT INTO people2 (pname,psurname)
-    #     VALUES ('Talha','Unsel'); 
-    # ''')
-    # mysql.connection.commit()
-
+@app.route('/')
+def index():
+    cur = mysql.connection.cursor()
 
     cur.execute('''
         SELECT * FROM PEOPLE2
@@ -42,4 +75,4 @@ def index():
     return str(resultarr)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=1999)
