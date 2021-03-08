38ea78eb29ad55dc9dd3b8c9a28aef0a53e3d21e
Ricider <sarp1999@gmail.com>
2021-03-08 23:58:18 +0300

    added queries for database insertions

--- flask/app.py
+++ flask/app.py
@@ -17,7 +17,7 @@ mysql = MySQL(app)
 def create_tables():
     cur = mysql.connection.cursor()
     
-    cur.execute('''CREATE TABLE IF NOT EXISTS people2 (
+    cur.execute('''CREATE TABLE IF NOT EXISTS people (
     person_id INT AUTO_INCREMENT PRIMARY KEY,
     pname VARCHAR(255) NOT NULL,
     psurname VARCHAR(255) NOT NULL,
@@ -25,8 +25,7 @@ def create_tables():
     );''')    
     
     cur.execute('''CREATE TABLE IF NOT EXISTS topics (
-    class_id INT AUTO_INCREMENT PRIMARY KEY,
-    class_name VARCHAR(255) NOT NULL,
+    class_name CHAR(6) PRIMARY KEY,
     class_desc VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );''')    
@@ -34,37 +33,46 @@ def create_tables():
     cur.execute('''CREATE TABLE IF NOT EXISTS classes (
     crn INT AUTO_INCREMENT PRIMARY KEY,
     c_teacher_id INT NOT NULL,
-    c_class_id INT NOT NULL,
+    c_class_name CHAR(6) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY(c_teacher_id)
     REFERENCES people2 (person_id),
-    FOREIGN KEY(c_class_id)
-    REFERENCES topics (class_id)
+    FOREIGN KEY(c_class_name)
+    REFERENCES topics (class_name)
     );''')   
     
     mysql.connection.commit()  
     return("success")
 
-@app.route('/add_user',methods = ["POST"])
-def add_user():
-    if request.method == "POST":
-        data = request.json
-        
+add_to_db_querries = {
+    "user": '''
+                INSERT INTO people (pname,psurname)
+                VALUES ('{}','{}'); 
+            ''' ,
+    "topic": '''
+                INSERT INTO topics (class_name,class_desc)
+                VALUES ('{}','{}'); 
+            ''' ,
+    "class": '''
+                INSERT INTO classes (c_class_name,c_teacher_id)
+                VALUES ('{}','{}'); 
+            ''' ,
+}
+
+@app.route('/add_<entry>',methods = ["POST"])
+def add_to_db(entry):
+    if request.method == "POST" and entry in ["user","class","topic"]:
         cur = mysql.connection.cursor()
-        cur.execute('''
-            INSERT INTO people2 (pname,psurname)
-            VALUES ('{}','{}'); 
-        '''.format(data["pname"],data["psurname"]))
-        mysql.connection.commit()        
-        
-        return "added user"
+        cur.execute(add_to_db_querries[entry].format(*request.json.values()))
+        mysql.connection.commit()
+        return add_to_db_querries[entry].format(*request.json.values())+"added "+entry
 
 @app.route('/')
 def index():
     cur = mysql.connection.cursor()
 
     cur.execute('''
-        SELECT * FROM PEOPLE2
+        SELECT * FROM PEOPLE
     ''')
 
     results = cur.fetchall()
