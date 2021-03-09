df3ef18bf4a685996f5f66e8ab17ebc52fb06d8a
Ricider <sarp1999@gmail.com>
2021-03-09 17:53:07 +0300

    added querries for retrieving class and student information

--- flask/app.py
+++ flask/app.py
@@ -27,6 +27,7 @@ def create_tables():
     cur.execute('''CREATE TABLE IF NOT EXISTS topics (
     class_name VARCHAR(6) PRIMARY KEY,
     class_desc VARCHAR(255),
+    credits INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );''')    
     
@@ -35,6 +36,7 @@ def create_tables():
     c_teacher_id INT NOT NULL,
     c_class_name VARCHAR(6) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
     FOREIGN KEY(c_teacher_id)
     REFERENCES people (person_id),
     FOREIGN KEY(c_class_name)
@@ -46,10 +48,27 @@ def create_tables():
     student_id INT NOT NULL,
     crn INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
     FOREIGN KEY(student_id)
     REFERENCES people (person_id),
     FOREIGN KEY(crn)
     REFERENCES classes (crn)
+    );''')   
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS grades (
+    grade_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT NOT NULL,
+    class_name VARCHAR(6) NOT NULL,
+    crn INT NOT NULL,
+    grade VARCHAR(2) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+    FOREIGN KEY(student_id)
+    REFERENCES people (person_id),
+    FOREIGN KEY(crn)
+    REFERENCES classes (crn),
+    FOREIGN KEY(class_name)
+    REFERENCES topics (class_name)
     );''')        
     
     mysql.connection.commit()  
@@ -61,8 +80,8 @@ add_to_db_querries = {
                 VALUES ('{}','{}'); 
             ''' ,
     "topic": '''
-                INSERT INTO topics (class_name,class_desc)
-                VALUES ('{}','{}'); 
+                INSERT INTO topics (class_name,class_desc,credits)
+                VALUES ('{}','{}',{}); 
             ''' ,
     "class": '''
                 INSERT INTO classes (c_class_name,c_teacher_id)
@@ -71,6 +90,10 @@ add_to_db_querries = {
     "enrollment": '''
                 INSERT INTO enrollment (student_id , crn)
                 VALUES ('{}','{}'); 
+            ''' ,
+    "grade": '''
+                INSERT INTO grades (student_id , class_name, grade, crn)
+                VALUES ('{}','{}','{}',{}); 
             ''' ,
 }
 
@@ -82,13 +105,79 @@ def add_to_db(entry):
         mysql.connection.commit()
         return "added "+entry
     
-@app.route('/<crn>/students',methods = ["GET"])
-def get_students(crn):
+def get_avg_grade(grades):
+    if not grades: return 0
+    grade_translation={"AA":4,"BA":3.5,"BB":3,"CB":2.5,"CC":2,"DC":1.5,"DD":1,"FF":0}
+    return sum([grade_translation[grade] for grade in [student["grade"] for student in grades]])/len(grades)
+    
+@app.route('/student_info/<student_id>',methods = ["GET"])
+def student_info(student_id):
     if request.method == "GET":
-        cur = mysql.connection.cursor()
+        cur = mysql.connection.cursor()   
+        
         cur.execute(
         '''
-        select person_id,pname,psurname,c_class_name from classes 
+        select classes.crn,c_class_name from classes 
+        inner join enrollment on 
+        classes.crn = enrollment.crn
+        inner join topics on
+        classes.c_class_name = topics.class_name
+        inner join people on 
+        people.person_id = enrollment.student_id
+        where people.person_id = {};
+        '''.format(student_id))
+        
+        classes = cur.fetchall()
+        
+        cur.execute(
+        '''
+        select student_id,grade from grades
+        inner join people on 
+        grades.student_id = people.person_id
+        where people.person_id = {};
+        '''.format(student_id))
+
+        grades = cur.fetchall()
+        
+        cur.execute(
+        '''
+        select pname,psurname from people where person_id = {}
+        '''.format(student_id))       
+        
+        info= cur.fetchone()
+        
+        return {"classes":classes,"grades":grades,"GPA":get_avg_grade(grades),"personal information":info}
+        
+@app.route('/crn_info/<crn>',methods = ["GET"])
+def crn_info(crn):
+    if request.method == "GET":
+        cur = mysql.connection.cursor()
+        
+        cur.execute(
+        '''
+        select pname as teacher_name,psurname as teacher_surname,person_id,c_class_name,class_desc,classes.crn from classes 
+        inner join people on 
+        classes.c_teacher_id = people.person_id
+        inner join topics on
+        classes.c_class_name = topics.class_name
+        where classes.crn = {};
+        '''.format(crn))
+        
+        info = cur.fetchone()
+        
+        cur.execute(
+        '''
+        select student_id,grade from grades
+        inner join people on 
+        grades.student_id = people.person_id
+        where grades.crn = {};
+        '''.format(crn))
+
+        grades = cur.fetchall()
+        
+        cur.execute(
+        '''
+        select person_id,pname,psurname from classes 
         inner join enrollment on 
         classes.crn = enrollment.crn
         inner join topics on
@@ -97,7 +186,10 @@ def get_students(crn):
         people.person_id = enrollment.student_id
         where classes.crn = {};
         '''.format(crn))
-        return {"students":cur.fetchall()}
+
+        students = cur.fetchall()
+                
+        return {"info": info,"students":students,"grades": grades,"class_average":get_avg_grade(grades)}
     
 @app.route('/')
 def index():
