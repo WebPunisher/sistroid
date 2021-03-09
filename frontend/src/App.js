6b78e7767641fa9affe20be108a92e4b3c6b1478
unsel <tunsel1@binghamton.edu>
2021-03-09 23:06:08 +0300

    initial dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Switch,Route} from 'react-router-dom';
 import SignIn from './pages/signin/signin.component';
 import Toolbar from './components/toolbar/toolbar.component';
 import HomePage from './pages/anotherpage/page.component';
+import Dashboard from './pages/dashboard/dashboard.component';
 
 import './App.css';
 
@@ -13,6 +14,7 @@ const App = props  =>{
       <Toolbar/>
       <Switch>
         <Route exact path='/'  component={HomePage}/>
+        <Route exact path='/dashboard'  component={Dashboard}/>
         <Route exact path='/sign'  component={SignIn}/>
       </Switch>
       </BrowserRouter>
