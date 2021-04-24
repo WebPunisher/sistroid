e71c0140c80ad6fb45f88c2172b016f9e0a39bc2
Ricider <sarp1999@gmail.com>
2021-04-24 20:47:16 +0300

    added histogram code for teacher-class pairs

--- flask/app.py
+++ flask/app.py
@@ -352,8 +352,8 @@ def get_class_histogram(class_name):
         
     return send_histogram(grades)
     
-@app.route('/person_histogram/<person_id>',methods = ["GET"])
-def get_person_histogram(person_id):
+@app.route('/teacher_histogram/<person_id>',methods = ["GET"])
+def get_teacher_histogram(person_id):
     cur = mysql.connection.cursor()     
     
     cur.execute("""
@@ -364,6 +364,28 @@ def get_person_histogram(person_id):
     people.person_id = %s 
     group by grade;
     """,(person_id,))
+    
+    grades={"AA":0,"BA":0,"BB":0,"CB":0,"CC":0,"DC":0,"DD":0,"FF":0}
+    data = cur.fetchall()
+    for row in data:
+        grades[row["grade"]] = row["count(*)"]
+        
+    return send_histogram(grades)
+    
+@app.route('/teacher_class_histogram/<person_id>/<class_id>',methods = ["GET"])
+def get_teacher_class_histogram(person_id,class_id):
+    cur = mysql.connection.cursor()     
+    
+    cur.execute("""
+    select grade,count(*) from grades,classes,people,topics
+    where
+    topics.class_name = classes.c_class_name and
+    classes.c_teacher_id = people.person_id and
+    grades.crn = classes.crn and
+    people.person_id = %s and
+    topics.class_name =  %s
+    group by grade;
+    """,(person_id,class_id))
     
     grades={"AA":0,"BA":0,"BB":0,"CB":0,"CC":0,"DC":0,"DD":0,"FF":0}
     data = cur.fetchall()
